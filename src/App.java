5d6e1362ab8303226111296044acb1d572769ebb
marcos <marcos.correa@docato.com.br>
2021-06-22 20:36:16 -0300

    2206

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  // import java.util.Scanner;
+  
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
     }
 }
